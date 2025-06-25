188bebb806357d21f34a0d85054dd219d716b19f
Griffin Gore <griffin.gore@outlook.com>
2025-06-25 19:47:05 -0400

    Added model for Printable objects

---
+++ 3DPrintStore/Models/Printable.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _3DPrintStore.Models
+{
+    public class Printable
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<string>? Tags { get; set; }
+        public string? Description { get; set; }
+        public string? URL { get; set; }
+        public bool? Verified { get; set; }
+        public List<string>? Filaments { get; set; }
+        public int Downloads { get; set; }
+        public decimal Rating { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
