4ca9172bb2d76c9139ab60960d95de88da81f509
Griffin Gore <griffin.gore@outlook.com>
2025-06-26 16:25:33 -0400

    Added data annotations to the Printable model

--- 3DPrintStore/Models/Printable.cs
+++ 3DPrintStore/Models/Printable.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace _3DPrintStore.Models
 {
     public class Printable
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Name { get; set; }
         public List<string>? Tags { get; set; }
+        [StringLength(500)]
         public string? Description { get; set; }
         public string? URL { get; set; }
+        [Required]
         public bool? Verified { get; set; }
         public List<string>? Filaments { get; set; }
         public int Downloads { get; set; }
         public decimal Rating { get; set; }
+        [Required]
         public int AuthorId { get; set; }
     }
 }
