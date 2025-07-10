3c22e4fadcbb396b5e0ec152601ed1b849a1c5bb
Griffin Gore <griffin.gore@outlook.com>
2025-07-10 16:46:46 -0400

    Added some files to start playing with React in a new directory

---
+++ React/dataTesting.js
@@ -0,0 +1,61 @@
+const printables = [
+    {
+        id: 1,
+        name: "Cube of Red",
+        tags: ["cool", "awesome", "insightful"],
+        imgURL: "https://picsum.photos/350/200",
+        verified: true,
+        filaments: ["PLA", "PETG", "ABS"],
+        downloads: 100,
+        rating: 4.5,
+        authorId: 1
+    },
+
+    {
+        id: 2,
+        name: "Not a Benchy",
+        tags: ["cool", "awesome", "insightful"],
+        imgURL: "https://picsum.photos/350/200",
+        verified: true,
+        filaments: ["PLA", "PETG", "ABS"],
+        downloads: 1000000,
+        rating: 4.5,
+        authorId: 1
+    },
+
+    {
+        id: 3,
+        name: "Fan Blades",
+        tags: ["cool", "awesome", "insightful"],
+        imgURL: "https://picsum.photos/350/200",
+        verified: true,
+        filaments: ["PLA", "PETG", "ABS"],
+        downloads: 1,
+        rating: 4.5,
+        authorId: 1
+    },
+
+    {
+        id: 4,
+        name: "A printer",
+        tags: ["cool", "awesome", "insightful"],
+        imgURL: "https://picsum.photos/350/200",
+        verified: true,
+        filaments: ["PLA", "PETG", "ABS"],
+        downloads: 10000000000,
+        rating: 4.5,
+        authorId: 1
+    },
+
+    {
+        id: 5,
+        name: "Battery Holder Crate for AA",
+        tags: ["cool", "awesome", "insightful"],
+        imgURL: "https://picsum.photos/350/200",
+        verified: true,
+        filaments: ["PLA", "PETG", "ABS"],
+        downloads: 1000,
+        rating: 4.5,
+        authorId: 1
+    }
+]
